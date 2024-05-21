248c519b4266c835017b7d3283a314ff64609ced
shagunp2e <140703211+shagunp2e@users.noreply.github.com>
2024-05-21 17:00:20 +0530

    second add function

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package gopackage
 
 import (
-	// "fmt"
+	"fmt"
 	"log"
 
 	
@@ -15,9 +15,22 @@ func LogWarn(msg string){
 	log.Printf("WARN- %v",msg)
 } 
 
-//erc20 like functions
-func Add(a int,b int)int{
-sum:= a+b
-return sum
-}
+// //erc20 like functions
+// func Add(a int,b int)int{
+// sum:= a+b
+// return sum
+// }
 
+
+// add two number checking for overflow
+func Add(b int, q int) (int, error) {
+ 
+    // Check overflow
+    var sum int = q + b
+ 
+    if (sum < q || sum < b) == (b >= 0 && q >= 0) {
+        return 0, fmt.Errorf("math: addition overflow occurred %d + %d", b, q)
+    }
+ 
+    return sum, nil
+}
